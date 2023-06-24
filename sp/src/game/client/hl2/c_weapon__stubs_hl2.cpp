150be06cb4d9ad8e2c308a3cd88ca0cbb4144709
Kralich <kralich10@gmail.com>
2023-06-24 22:11:01 +0000

    c_weapon__stubs_hl2.cpp edited to fix stubs?

--- sp/src/game/client/hl2/c_weapon__stubs_hl2.cpp
+++ sp/src/game/client/hl2/c_weapon__stubs_hl2.cpp
@@ -42,13 +42,13 @@ STUB_WEAPON_CLASS( weapon_mp40, WeaponMP40, C_HLSelectFireMachineGun );
 STUB_WEAPON_CLASS( weapon_mp7, WeaponMP7, C_HLSelectFireMachineGun );
 STUB_WEAPON_CLASS( weapon_357, Weapon357, C_BaseHLCombatWeapon );
 STUB_WEAPON_CLASS( weapon_crossbow, WeaponCrossbow, C_BaseHLCombatWeapon );
+STUB_WEAPON_CLASS( weapon_billyclub, WeaponBillyclub, C_BaseHLBludgeonWeapon );
+STUB_WEAPON_CLASS( weapon_shovel, WeaponShovel, C_BaseHLBludgeonWeapon );
+STUB_WEAPON_CLASS( weapon_nightstick, WeaponNightstick, C_BaseHLBludgeonWeapon );
 #ifndef MAPBASE
 STUB_WEAPON_CLASS( weapon_slam, Weapon_SLAM, C_BaseHLCombatWeapon );
 #endif
 STUB_WEAPON_CLASS( weapon_crowbar, WeaponCrowbar, C_BaseHLBludgeonWeapon );
-STUB_WEAPON_CLASS( weapon_billyclub, WeaponBillyclub, C_BaseHLBludgeonWeapon );
-STUB_WEAPON_CLASS( weapon_shovel, WeaponShovel, C_BaseHLBludgeonWeapon );
-STUB_WEAPON_CLASS( weapon_nightstick, WeaponNightstick, C_BaseHLBludgeonWeapon );
 #ifdef HL2_EPISODIC
 STUB_WEAPON_CLASS( weapon_hopwire, WeaponHopwire, C_BaseHLCombatWeapon );
 //STUB_WEAPON_CLASS( weapon_proto1, WeaponProto1, C_BaseHLCombatWeapon );
